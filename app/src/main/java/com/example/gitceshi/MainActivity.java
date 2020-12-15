c931a3869a1c792a7cbeb8100711b5ee17d425b5
2692996364 <70733939+2692996364@users.noreply.github.com>
2020-12-15 19:51:41 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/gitceshi/MainActivity.java
+++ app/src/main/java/com/example/gitceshi/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		//			 已核杂诗
+		//	    已亥杂诗
 		//浩荡离愁白日斜，吟鞭东指即天涯。
 		//落红不是无情物，化作春泥更护花。
     }
