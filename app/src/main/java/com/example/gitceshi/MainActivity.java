50de14f47b903359e2847d1be7ddc6b872e21222
2692996364 <2692996364@qq.com>
2020-12-15 19:46:19 +0800

    提交信息

--- app/src/main/java/com/example/gitceshi/MainActivity.java
+++ app/src/main/java/com/example/gitceshi/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		//			 已核杂诗
+		//浩荡离愁白日斜，吟鞭东指即天涯。
+		//落红不是无情物，化作春泥更护花。
     }
 }
